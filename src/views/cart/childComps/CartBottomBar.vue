5cfc74d12f7b261bfffcfa1a8a711e7460419fb6
kaixingg <1536863186@qq.com>
2022-07-27 23:33:01 +0800

    ok

---
+++ src/views/cart/childComps/CartBottomBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bottom-bar">
+    <button type="checkbox"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CartBottomBar',
+  props: {},
+  data () {
+    return {
+
+    }
+  },
+  // 这时可以访问data和methods
+  created () { },
+  // 方法
+  methods: {},
+  // 计算属性
+  computed: {},
+  // 组件注册
+  components: {},
+}
+</script>
+
+<style scoped>
+.bottom-bar {
+  height: 40px;
+  background-color: pink;
+  position: relative;
+}
+</style>
