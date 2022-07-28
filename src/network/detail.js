bab4b3bc84d03f57e1dfd9c37c070cbf14419eed
kaixingg <1536863186@qq.com>
2022-07-28 14:44:15 +0800

    update

---
+++ src/network/detail.js
@@ -0,0 +1,45 @@
+import { request, newRequest } from './request'
+export function getDetail (iid) {
+  return newRequest({
+    url: '/detail',
+    params: {
+      iid
+    }
+  })
+}
+// 把服务端返回的数据封装到一个类里面,再创建一个类的实例
+export class Goods {
+  constructor(itemInfo, columns, services) {
+    this.title = itemInfo.title
+    this.desc = itemInfo.desc
+    this.newPrice = itemInfo.price
+    this.oldPrice = itemInfo.oldPrice
+    this.discount = itemInfo.discountDesc
+    this.columns = columns
+    this.services = services
+    this.realPrice = itemInfo.lowNowPrice
+  }
+}
+
+// 提取shop信息
+export class Shop {
+  constructor(shopInfo) {
+    this.logo = shopInfo.shopLogo
+    this.name = shopInfo.name
+    this.fans = shopInfo.cFans
+    this.sells = shopInfo.cSells
+    this.score = shopInfo.score
+    this.goodsCount = shopInfo.cGoods
+  }
+}
+
+export class GoodsParam {
+  constructor(info, rule) {
+    // 注: images可能没有值(某些商品有值, 某些没有值)
+    this.image = info.images ? info.images[0] : ''
+    this.infos = info.set
+    this.sizes = rule.tables
+  }
+}
+
+
